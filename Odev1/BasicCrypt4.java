9a07988a3b4fa292513ca2c5db082bb060e87d6e
atakanemre <atakan21@outlook.com>
2023-10-19 20:49:43 +0300

    ADD PDF

--- Odev1/BasicCrypt4.java
+++ Odev1/BasicCrypt4.java
@@ -1,75 +1,38 @@
-//package Odev1;
-
-// Kullanıcıdan giriş almak için gereken sınıfı dahil ediyoruz.
+package Odev1;
 import java.util.Scanner;
 
 public class BasicCrypt4 {
 
-    // Şifreleme fonksiyonu
     public static String encrypt(String text) {
-        // Şifrelenmiş metni oluşturmak için bir StringBuilder nesnesi tanımlıyoruz.
-        // StringBuilder, metni etkili bir şekilde değiştirmek için kullanılır.
-        StringBuilder encryptedText = new StringBuilder();
+        char[] encryptedText = new char[text.length()];
 
-        // Orijinal metnin ilk karakterini şifrelenmiş metne doğrudan ekliyoruz.
-        encryptedText.append(text.charAt(0));
-
-        // Orijinal metnin uzunluğunun bir eksiğine kadar bir döngü başlatıyoruz.
-        for (int i = 0; i < text.length() - 1; i++) {
-            // Şu anki karakteri alıyoruz.
+        for (int i = 0; i < text.length(); i++) {
             char charAtI = text.charAt(i);
-            // Bir sonraki karakteri alıyoruz.
-            char nextChar = text.charAt(i + 1);
-
-            // İki ardışık karakterin ASCII değerlerinin farkını alıyoruz.
-            // 256'ya eklememizin sebebi, negatif bir değer elde etmekten kaçınmaktır.
-            char encryptedChar = (char) ((nextChar - charAtI + 256) % 256);
-            // Şifrelenmiş karakteri sonuca ekliyoruz.
-            encryptedText.append(encryptedChar);
+            encryptedText[i] = (char) (charAtI + (i + 1) * (i + 1));  // ASCII değerine sırasının karesini ekliyoruz.
         }
 
-        // Oluşturulan şifrelenmiş metni geri döndürüyoruz.
-        return encryptedText.toString();
+        return new String(encryptedText);
     }
 
-    // Deşifreleme fonksiyonu
     public static String decrypt(String encryptedText) {
-        // Deşifre edilmiş metni oluşturmak için bir StringBuilder nesnesi tanımlıyoruz.
-        StringBuilder decryptedText = new StringBuilder();
-        // Şifrelenmiş metnin ilk karakterini deşifre edilmiş metne doğrudan ekliyoruz.
-        decryptedText.append(encryptedText.charAt(0));
+        char[] decryptedText = new char[encryptedText.length()];
 
-        // Şifrelenmiş metnin uzunluğuna kadar bir döngü başlatıyoruz.
-        for (int i = 1; i < encryptedText.length(); i++) {
-            // Deşifre edilmiş metinden şu anki karakteri alıyoruz.
-            char charAtI = decryptedText.charAt(i - 1);
-            // Şifrelenmiş metinden şu anki karakteri alıyoruz.
+        for (int i = 0; i < encryptedText.length(); i++) {
             char encryptedChar = encryptedText.charAt(i);
-
-            // Orijinal karakteri geri almak için şifrelenmiş karakteri şu anki karakterle topluyoruz.
-            char decryptedChar = (char) ((charAtI + encryptedChar) % 256);
-            // Deşifre edilmiş karakteri sonuca ekliyoruz.
-            decryptedText.append(decryptedChar);
+            decryptedText[i] = (char) (encryptedChar - (i + 1) * (i + 1));  // ASCII değerinden sırasının karesini çıkarıyoruz.
         }
 
-        // Oluşturulan deşifre edilmiş metni geri döndürüyoruz.
-        return decryptedText.toString();
+        return new String(decryptedText);
     }
 
-    // Ana fonksiyon
     public static void main(String[] args) {
-        // Kullanıcıdan metin girişi almak için bir Scanner nesnesi oluşturuyoruz.
         Scanner scanner = new Scanner(System.in);
         System.out.print("Lütfen şifrelemek istediğiniz metni girin: ");
-        // Kullanıcının girdiği metni okuyoruz.
         String originalText = scanner.nextLine();
 
-        // Orijinal metni şifreliyoruz.
         String encrypted = encrypt(originalText);
-        // Şifrelenmiş metni deşifre ediyoruz.
         String decrypted = decrypt(encrypted);
 
-        // Sonuçları ekrana yazdırıyoruz.
         System.out.println("Orijinal Metin: " + originalText);
         System.out.println("Şifrelenmiş Metin: " + encrypted);
         System.out.println("Deşifre Edilmiş Metin: " + decrypted);
